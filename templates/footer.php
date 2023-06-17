b3d8121df27c25dc9039259b439eaddaa888fb17
josephredmond5 <josephredmond5@icloud.com>
2023-06-17 06:07:35 +0100

    header and footer added

---
+++ templates/footer.php
@@ -0,0 +1,5 @@
+
+    <footer class="section">
+        <div class="center grey-text">copywright ninas pizza </div>
+    </footer>
+</body>
---
+++ templates/header.php
@@ -0,0 +1,27 @@
+<head>
+    <title>ninja Pizza</title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <style type="text/css">
+        .brand{
+            background-color: #cbb09c !important;
+        }
+        .brand-text{
+            color: #cbb09c !important;
+        }
+    </style>
+            
+</head>
+    <body class="grey lighten-4">
+        <nav class="white z-depth-0">
+            <div class="container">
+                <a href="#" class="brand-logo brand-text">Ninjas Pizza</a>
+                <ul id="nav-mobile" class="right hide-on-small-and-down">
+                    <li><a href="#" class="btn brand z-depth-0">Add a Pizza</a> </li>
+                </ul>
+            </div>
+        </nav>
+
