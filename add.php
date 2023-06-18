1952d95142dbadf0546a596649ebd6fbf2391cca
josephredmond5 <josephredmond5@icloud.com>
2023-06-18 20:59:57 +0100

    input fields are working correctly

--- add.php
+++ add.php
@@ -1,19 +1,48 @@
 <?php 
 
 // the form detauils will be stored in the GET array
-if(isset($_GET['submit'])){
-   echo $_GET['email'];
-   echo $_GET['title'];
-   echo $_GET['Ingridients']; 
-} 
+// if(isset($_GET['submit'])){
+//    echo $_GET['email'];
+//    echo $_GET['title'];
+//    echo $_GET['Ingridients']; 
+// } 
 
-if(isset($_POST['submit'])){
-    echo $_POST['email'];
-    echo $_POST['title'];
-    echo $_POST['Ingridients']; 
- } 
+if(isset($_POST['submit'])){ // this is to stop hackers destryoing this
+    // echo htmlspecialchars($_POST['email']);
+    // echo htmlspecialchars($_POST['title']);
+    // echo htmlspecialchars($_POST['Ingridients']); 
 
+        // check email
+    if(empty($_POST['email'])){ // this is checking if there has been any info put in the input fields
+        echo 'an email is required <br />';
+    } else {
+        $email = $_POST['email'];
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){ // this filter is built into PHP 
+           ECHO 'email must be a valid email address'; // this whole IF statement checks if the email is valid
+        }
+    }
+        // check title
+    if(empty($_POST['title'])){
+        echo 'a title is required <br />';
+    } else {
+        $title = $_POST['title'];
+        if(!preg_match('/⌃[a-zA-Z\s]+$/', $title)){
+            echo 'Title must be letters and spaces only';
+        }
+    }
 
+        // check ingridients
+        if (empty($_POST['Ingridients'])) { // check if the ingridients are comma seperated
+            echo 'At least 1 ingredient is required <br />';
+        } else {
+            $Ingridients = $_POST['Ingridients'];
+            if (!preg_match('/⌃[a-zA-Z\s]+(,\s*[a-zA-Z\s]*)*$/', $Ingridients)) {
+                echo 'Ingredients must be a comma seperated list';
+            }
+        }
+    }        
+
+    // end of POST check
 
 ?>
 
