791dd4878e0baec7ad5c26baa7f346e8affa1d1f
josephredmond5 <josephredmond5@icloud.com>
2023-06-24 19:10:35 +0100

    getting data from a database

--- index.php
+++ index.php
@@ -8,6 +8,25 @@ if(!$conn){
     echo 'connection error: ' . mysqli_connect();
 }
 
+// write query for all pizzas
+$sql = 'SELECT title, ingredients, id FROM pizza';
+
+// make query and get result
+$result = mysqli_query($conn, $sql);
+
+// fetch the resulting rows as an array 
+$pizza = mysqli_fetch_all($result, MYSQLI_ASSOC); // saying we want this back as an associative array
+
+
+mysqli_free_result($result); // this is freeing the resulting from memory
+
+// close connection to the database
+mysqli_close($conn);
+
+print_r($pizza);
+
+
+
 ?>
 
 
